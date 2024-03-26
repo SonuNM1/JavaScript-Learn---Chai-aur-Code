b24999bf32ecc46ac2e1c8df3be72fe59efd0b0d
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-03-26 18:49:08 +0530

    advance JS

---
+++ Advance JS/API/promises.js
@@ -0,0 +1,64 @@
+
+const promiseOne = new Promise(function(resolve, reject){
+    // do an async task
+    // DB calls, cryptogrpahy, network calls, file handling
+
+    setTimeout(function(){
+        console.log('Async task completed') ; 
+        resolve() ; 
+    },5000)
+
+}) ; 
+
+promiseOne.then(function(){
+    console.log('promise consumed') ; 
+})
+
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('Async task 2') ; 
+        resolve() ; 
+    },1000);
+}).then(function(){
+    console.log('Promise two resolved') ; 
+})
+
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"}) ;
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user) ; 
+})
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false ; 
+        if(!error){
+            resolve({username: "SonuNM", password: "123"}) ; 
+        } else{
+            reject('ERROR: Something went wrong') ; 
+        }
+    }, 2000);
+})
+
+
+const username = promiseFour.
+then((user)=>{
+    console.log(user) ; 
+    return user.username ;
+}).then((username)=>{
+    console.log(username) ; 
+}).catch(function(error){
+    console.log(error) ; 
+}).finally(()=>{
+    console.log('The promise is either resolved or rejected') ; 
+})
+
+console.log(username);
+
