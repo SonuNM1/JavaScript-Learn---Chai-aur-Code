3fbb5175abfa82cd8214c523969a482a4553ed45
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-03-28 22:17:52 +0530

    28th March

--- object oriented programming/oop.js
+++ object oriented programming/oop.js
@@ -26,9 +26,33 @@ console.log(this)
 */
 
 
-
-
-
                                     // CONSTRUCTOR FUNCTION
 
 
+
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+
+function User(username, loginCount, isLoggedIn){
+    // lhs -> variable rhs -> value that you are passing
+    this.username = username ; 
+    this.loginCount = loginCount ; 
+    this.isLoggedIn = isLoggedIn ;
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`) ; 
+    }
+
+    // return this ; (implictly defined)
+}
+
+const userOne = new User('SonuNM', 12, true)
+const userTwo = new User('Chai aur code', 11, true)
+
+console.log(userOne.constructor)
+console.log(userTwo)
+
+
+                   
